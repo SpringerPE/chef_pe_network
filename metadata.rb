2f3acad364ae08e1b204488c537a383d7b322034
jose.riguera@springer.com <jose.riguera@springer.com>
2014-07-25 10:20:12 +0200

    creation

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'pe_network'
+maintainer       'Springer'
+maintainer_email 'jose.riguera@springer.com'
+license          'Apache 2.0'
+description      'Installs/Configures pe_network'
+long_description 'Installs/Configures pe_network'
+version          '0.1.0'
+
