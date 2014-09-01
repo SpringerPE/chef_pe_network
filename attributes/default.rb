0a2f3760a30108128d8620dc648b1a3d42fb2cdf
Jose Riguera <jriguera@gmail.com>
2014-09-01 18:25:13 +0200

    added new attr to control fw runs

--- attributes/default.rb
+++ attributes/default.rb
@@ -21,7 +21,8 @@ default[:pe_network][:network] = {
 #      :mac => "08:00:27:84:ee:e7"
 #  }
 }
-
+# Do not run the FW recipe by default
+default[:pe_network][:run_fw] = false
 
 ### Firewall
 default[:pe_network][:firewall][:enabled] = true
